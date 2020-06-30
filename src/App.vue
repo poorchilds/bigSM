e8a1ed90ff13e147c92a8f6e6f4a8cf3209d779d
qiuchao <7465892+qiu-chao@user.noreply.gitee.com>
2020-06-30 15:36:49 +0800

    初次提交

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class=''>
+    我是大主件
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'MyComponent',
+  props: { },
+  data () {
+    return { }
+  },
+  computed: { },
+  created () { },
+  mounted () { }
 }
+</script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style scoped lang='less'></style>
